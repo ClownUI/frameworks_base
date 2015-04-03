9e349719d54672c416d061c29bdb6e191b399cc1
Michael Bestas <mikeioannina@gmail.com>
2015-04-03 14:33:37 -0700

    Forward port CM Screen Security settings [1/2]
     * Variable size pattern lockscreen
     * Toggle dots/error pattern visibility
    
    Change-Id: Ie109e82c1fb2fd96b07e977e1cd76ae3acb865ff
    
    Fix pattern visibility settings (1/2)
    
    Change-Id: Ic627953c5df854c442671a98b5da539b994da18b
    
    LockPatternUtils: Use the actual user id to set pattern size
    
    Ticket: CYNGNOS-2462
    Change-Id: Ia68e26ec2dfc23317135d933bc25204c1380bb02
    
    LockSettings: fix build
    
    Change-Id: Ic65b1a2af398faffb83776ee4013d47f79ab6619
    Signed-off-by: Roman Birg <roman@cyngn.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- packages/SystemUI/tests/utils/src/com/android/systemui/authentication/data/repository/FakeAuthenticationRepository.kt
+++ packages/SystemUI/tests/utils/src/com/android/systemui/authentication/data/repository/FakeAuthenticationRepository.kt
@@ -52,6 +52,9 @@ class FakeAuthenticationRepository(
     override val isAutoConfirmFeatureEnabled: StateFlow<Boolean> =
         _isAutoConfirmFeatureEnabled.asStateFlow()
 
+    private val _patternSize = MutableStateFlow(LockPatternUtils.PATTERN_SIZE_DEFAULT)
+    override val patternSize: StateFlow<Byte> = _patternSize.asStateFlow()
+
     private val _authenticationMethod =
         MutableStateFlow<AuthenticationMethodModel>(DEFAULT_AUTHENTICATION_METHOD)
     override val authenticationMethod: StateFlow<AuthenticationMethodModel> =
@@ -242,7 +245,8 @@ class FakeAuthenticationRepository(
                 isPattern ->
                     credential.contentEquals(
                         LockPatternUtils.patternToByteArray(
-                            expectedCredential as List<LockPatternView.Cell>
+                            expectedCredential as List<LockPatternView.Cell>,
+                            LockPatternUtils.PATTERN_SIZE_DEFAULT
                         )
                     )
                 else -> error("Unsupported credential type $type!")
@@ -250,7 +254,8 @@ class FakeAuthenticationRepository(
         }
 
         private fun List<AuthenticationPatternCoordinate>.toCells(): List<LockPatternView.Cell> {
-            return map { coordinate -> LockPatternView.Cell.of(coordinate.y, coordinate.x) }
+            return map { coordinate -> LockPatternView.Cell.of(
+                coordinate.y, coordinate.x, LockPatternUtils.PATTERN_SIZE_DEFAULT) }
         }
     }
 }
