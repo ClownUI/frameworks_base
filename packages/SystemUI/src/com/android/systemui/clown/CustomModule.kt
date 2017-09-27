2e63148c066f3b3e0a314ceac9ee285222cd07a6
Roman Birg <roman@cyngn.com>
2017-09-27 21:01:50 +0200

    SystemUI: Add Compass tile
    @neobuddy89:
    * Set default state to inactive
    * Remove slashed icon
    * Add check if device supports compass
    * Disable long click for Compass tile
    * Code clean up and adapt to Android 13, 14
    * Combine compass check in tile itself
    
    Squashed:
    
        From: timjosten <timjosten@gmail.com>
        Date: Thu, 18 Aug 2022 18:10:47 +0000
        Subject: SystemUI: Fix QS compass tile spam & instability
    
        E ViewRootImpl: Accessibility content change on non-UI thread. Future Android versions will throw an exception.
        E ViewRootImpl: android.view.ViewRootImpl$CalledFromWrongThreadException: Only the original thread that created a view hierarchy can touch its views.
        E ViewRootImpl:  at android.view.ViewRootImpl$SendWindowContentChangedAccessibilityEvent.runOrPost(ViewRootImpl.java:10375)
        E ViewRootImpl:  at android.view.ViewRootImpl.postSendWindowContentChangedCallback(ViewRootImpl.java:9203)
        E ViewRootImpl:  at android.view.ViewRootImpl.notifySubtreeAccessibilityStateChanged(ViewRootImpl.java:9383)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.ViewGroup.notifySubtreeAccessibilityStateChanged(ViewGroup.java:3847)
        E ViewRootImpl:  at android.view.View.notifySubtreeAccessibilityStateChangedIfNeeded(View.java:14002)
        E ViewRootImpl:  at android.view.View.setRotation(View.java:17015)
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- packages/SystemUI/src/com/android/systemui/clown/CustomModule.kt
+++ packages/SystemUI/src/com/android/systemui/clown/CustomModule.kt
@@ -20,6 +20,7 @@ import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.AutoBrightnessTile
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.CellularTile
+import com.android.systemui.qs.tiles.CompassTile
 import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.WifiTile
 import com.android.systemui.qs.tiles.HeadsUpTile
@@ -46,6 +47,12 @@ interface CustomModule {
     @IntoMap
     @StringKey(CellularTile.TILE_SPEC)
     fun bindCellularTile(cellularTile: CellularTile): QSTileImpl<*>
+
+    /** Inject CompassTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CompassTile.TILE_SPEC)
+    fun bindCompassTile(compassTile: CompassTile): QSTileImpl<*>
 
     /** Inject DataSwitchTile into tileMap in QSModule */
     @Binds
