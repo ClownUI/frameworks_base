20ee657b42f8db149e3b18349d80e433bfe318b2
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-18 20:33:08 -0400

    SystemUI: Status bar tuner improvements
    * Split icon toggles to a separate page
    * Remove deprecated TunerService usage
    * Match theme with Settings
    
    idoybh (YAAP):
    * SwitchPreference -> SwitchPreferenceCompat fixes
    * Fix insets
    * Fixes for A15 theme
    
    Co-authored-by: Gabriele M <moto.falcon.git@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: Oliver Scott <olivercscott@gmail.com>
    Co-authored-by: SagarMakhar <sagarmakhar@gmail.com>
    Co-authored-by: Steve Kondik <steve@cyngn.com>
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Change-Id: Icc84575db18931d3a43bd9e640f2a55a51b460c1
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarSignalPolicy.java
@@ -53,6 +53,7 @@ public class StatusBarSignalPolicy implements SignalCallback,
     private final String mSlotVpn;
     private final String mSlotNoCalling;
     private final String mSlotCallStrength;
+    private final String mSlotRoaming = "roaming";
 
     private final Context mContext;
     private final StatusBarIconController mIconController;
@@ -66,6 +67,8 @@ public class StatusBarSignalPolicy implements SignalCallback,
     private boolean mHideMobile;
     private boolean mHideEthernet;
     private boolean mActivityEnabled;
+    private boolean mHideVpn;
+    private boolean mHideRoaming;
 
     // Track as little state as possible, and only for padding purposes
     private boolean mIsAirplaneMode = false;
@@ -118,7 +121,7 @@ public class StatusBarSignalPolicy implements SignalCallback,
     }
 
     private void updateVpn() {
-        boolean vpnVisible = mSecurityController.isVpnEnabled();
+        boolean vpnVisible = mSecurityController.isVpnEnabled() && !mHideVpn;
         int vpnIconId = currentVpnIconId(
                 mSecurityController.isVpnBranded(),
                 mSecurityController.isVpnValidated());
@@ -157,12 +160,17 @@ public class StatusBarSignalPolicy implements SignalCallback,
         boolean hideAirplane = hideList.contains(mSlotAirplane);
         boolean hideMobile = hideList.contains(mSlotMobile);
         boolean hideEthernet = hideList.contains(mSlotEthernet);
+        boolean hideVpn = hideList.contains(mSlotVpn);
+        boolean hideRoaming = hideList.contains(mSlotRoaming);
 
         if (hideAirplane != mHideAirplane || hideMobile != mHideMobile
-                || hideEthernet != mHideEthernet) {
+                || hideEthernet != mHideEthernet || hideVpn != mHideVpn
+                || hideRoaming != mHideRoaming) {
             mHideAirplane = hideAirplane;
             mHideMobile = hideMobile;
             mHideEthernet = hideEthernet;
+            mHideVpn = hideVpn;
+            mHideRoaming = hideRoaming;
             // Re-register to get new callbacks.
             mNetworkController.removeCallback(this);
             mNetworkController.addCallback(this);
