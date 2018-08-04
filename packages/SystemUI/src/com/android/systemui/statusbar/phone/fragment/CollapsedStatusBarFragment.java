0cfb11ddf08ebb4a392d21365fb523d0e5b0c00d
Pranav Vashi <neobuddy89@gmail.com>
2018-08-04 23:07:56 +0530

    Use position tunable to hide clock as well
    * There is no need for separate status bar tuner switch for this.
    
    Squashed:
    
        From: minaripenguin <minaripenguin@users.noreply.github.com>
        Date: Mon, 9 Sep 2024 15:44:44 +0800
        Subject: [PATCH] ClockController: Prevent possible memory leak
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: I9a1aa5870261983c0127a73c54fb03090c750e71
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/fragment/CollapsedStatusBarFragment.java
@@ -446,6 +446,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         super.onPause();
         mCommandQueue.removeCallback(this);
         mStatusBarStateController.removeCallback(this);
+        mClockController.removeTunable();
         mOngoingCallController.removeCallback(mOngoingCallListener);
         mAnimationScheduler.removeCallback(this);
         mSecureSettings.unregisterContentObserverSync(mVolumeSettingObserver);
@@ -565,6 +566,9 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             updateNotificationIconAreaAndOngoingActivityChip(animate);
         }
 
+        if (mClockController.getClock() == null)
+            return;
+
         // The clock may have already been hidden, but we might want to shift its
         // visibility to GONE from INVISIBLE or vice versa
         if (newModel.getShowClock() != previousModel.getShowClock()
@@ -589,13 +593,15 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
                 && !(mStatusBarStateController.getState() == StatusBarState.KEYGUARD
                         && headsUpVisible)) {
             View clockView = mClockController.getClock();
-            boolean isRightClock = clockView.getId() == R.id.clock_right;
-            // Hide everything
-            return new StatusBarVisibilityModel(
-                    /* showClock= */ isRightClock,
-                    /* showNotificationIcons= */ false,
-                    /* showOngoingActivityChip= */ false,
-                    /* showSystemInfo= */ false);
+            if (clockView != null) {
+                boolean isRightClock = clockView.getId() == R.id.clock_right;
+                // Hide everything
+                return new StatusBarVisibilityModel(
+                        /* showClock= */ isRightClock,
+                        /* showNotificationIcons= */ false,
+                        /* showOngoingActivityChip= */ false,
+                        /* showSystemInfo= */ false);
+            }
         }
 
         boolean showClock = externalModel.getShowClock() && !headsUpVisible;
@@ -756,6 +762,9 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
      * don't set the clock GONE otherwise it'll mess up the animation.
      */
     private int clockHiddenMode() {
+        if (mClockController.getClock() == null)
+            return View.GONE;
+
         if (!mShadeExpansionStateManager.isClosed() && !mKeyguardStateController.isShowing()
                 && !mStatusBarStateController.isDozing()
                 && mClockController.getClock().shouldBeVisible()) {
@@ -807,6 +816,8 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
      * Hides a view.
      */
     private void animateHide(final View v, boolean animate) {
+        if (v == null)
+            return;
         animateHiddenState(v, View.INVISIBLE, animate);
     }
 
@@ -814,6 +825,8 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
      * Shows a view, and synchronizes the animation with Keyguard exit animations, if applicable.
      */
     private void animateShow(View v, boolean animate) {
+        if (v == null)
+            return;
         v.animate().cancel();
         v.setVisibility(View.VISIBLE);
         if (!animate || !mAnimationsEnabled) {
