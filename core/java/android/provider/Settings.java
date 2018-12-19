61ef5f3cf20be2b93885a44d55daac7929e427ab
a.derendyaev <a.derendyaev@magdv.com>
2018-12-19 21:57:45 +0800

    SystemUI: runtime configurable audio panel location
    Co-authored-by: Alex Cruz <alex@dirtyunicorns.com>
    Co-authored-by: Arian <arian.kulmer@web.de>
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Co-authored-by: programminghoch10 <hoch10@kabelbw.de>
    Co-authored-by: Sam Mortimer <sam@mortimer.me.uk>
    Change-Id: If22c97eb1ce4cfb13396d182d90786f4b7acaee7
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12577,6 +12577,14 @@ public final class Settings {
                 "com.android.server.display.HBM_SETTING_KEY";
 
         /**
+         * Whether volume panel should appear on the left (or right).
+         * 0 = false (on the right)
+         * 1 = true (on the left)
+         * @hide
+         */
+        public static final String VOLUME_PANEL_ON_LEFT = "volume_panel_on_left";
+        
+        /**
          * Whether to use black theme for dark mode
          * @hide
          */
