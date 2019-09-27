0b1481d6e8f1002284c408d8e88c21e847d1ba25
maxwen <max.weninger@gmail.com>
2019-09-27 23:13:28 +0200

    base: add CPU info overlay
    includes all modifications and fixes from P
    
    @neobuddy89: Remove redundant DreamManager code
    
    Change-Id: I0e6dee53ebcd864a522cfd3d78a5abb253991a9c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <Kutemeikito0905@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11208,6 +11208,13 @@ public final class Settings {
          */
         public static final String BERRY_BLACK_THEME = "berry_black_theme";
 
+
+        /**
+         * Control whether the process CPU info meter should be shown.
+         * @hide
+         */
+        public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
