16e1910f8cb5d96c5b082d9e2a4df35f63656dff
ezio84 <brabus84@gmail.com>
2021-12-03 19:51:29 +0530

    base: add API for disabling gestural navigation
    [jhonboy121]: adapted to A12L and A13
    [neobuddy89]: updated for A14
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -647,6 +647,8 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
 
     private boolean mShowDimissButton;
 
+    private boolean mBlockedGesturalNavigation = false;
+
     private final SplitShadeStateController mSplitShadeStateController;
     private final Runnable mFlingCollapseRunnable = () -> fling(0, false /* expand */,
             mNextCollapseSpeedUpFactor, false /* expandBecauseOfFalsing */);
@@ -3724,6 +3726,11 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     }
 
     @Override
+    public void setBlockedGesturalNavigation(boolean blocked) {
+        mBlockedGesturalNavigation = blocked;
+    }
+
+    @Override
     public void updateSystemUiStateFlags() {
         if (SysUiState.DEBUG) {
             Log.d(TAG, "Updating panel sysui state flags: fullyExpanded="
@@ -3735,7 +3742,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                 .setFlag(SYSUI_STATE_NOTIFICATION_PANEL_EXPANDED,
                         isFullyExpanded() && !mQsController.getExpanded())
                 .setFlag(SYSUI_STATE_QUICK_SETTINGS_EXPANDED,
-                        isFullyExpanded() && mQsController.getExpanded()).commitUpdate(mDisplayId);
+                        mBlockedGesturalNavigation || (isFullyExpanded() && mQsController.getExpanded())).commitUpdate(mDisplayId);
     }
 
     private void debugLog(String fmt, Object... args) {
