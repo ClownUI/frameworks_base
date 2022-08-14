246f333900240cffd1538b34f7f05959f122f775
LynnrinChan <wuxiaofei31@gmail.com>
2022-08-14 11:20:02 +0800

    fixup! base: Add status bar lyric [1/3]
     * java.lang.SecurityException: Settings key: <status_bar_show_lyric> is
       not readable. From S+, settings keys annotated with @hide are
       restricted to system_server and system apps only, unless they are
       annotated with @Readable.
    
    Change-Id: Ia0c209f3ddd5bbe1be676e172f408b3faf8cfe81
    Signed-off-by: Edwiin Kusuma Jaya <Kutemeikito0905@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11367,6 +11367,7 @@ public final class Settings {
          *
          * @hide
          */
+        @Readable
         public static final String STATUS_BAR_SHOW_LYRIC = "status_bar_show_lyric";
 
         /**
