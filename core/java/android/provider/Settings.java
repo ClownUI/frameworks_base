51b6c9b7d26b7f4f121f9a593ef996504545146a
LuK1337 <priv.luk@gmail.com>
2022-11-02 12:59:32 +0100

    camera: Allow extending aux list/excludelist via resources
    Properties are limited and don't allow us to list as many apps as we
    wish.
    
    Change-Id: I4432f627f1fa120b6ea41c252cd512beb129848c
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -121,7 +121,7 @@ import java.util.Set;
 import java.util.concurrent.Executor;
 import java.util.function.Consumer;
 
-import com.android.internal.util.custom.DeviceConfigUtils;
+import com.android.internal.util.clown.DeviceConfigUtils;
 
 /**
  * The Settings provider contains global system-level device preferences.
