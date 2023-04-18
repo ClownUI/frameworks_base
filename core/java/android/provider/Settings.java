5c6fa866a0871b16dfc2e96b684cc916d10fe9d8
Pranav Vashi <neobuddy89@gmail.com>
2023-04-18 23:24:48 +0530

    base: Add switch to unlink ringtone and notifications volume [1/2]
    * Make use of AOSP config to set default initial value
    
    Ref old LOS implementation:
    https://github.com/crdroidandroid/android_frameworks_base/commit/345ff7b8618bd57f6c6ccbf875cf860338deb513
    https://github.com/crdroidandroid/android_frameworks_base/commit/ab1aad9aa76391d675ca38a93f23120032c1d957
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11012,6 +11012,12 @@ public final class Settings {
         public static final String ADAPTIVE_CONNECTIVITY_ENABLED = "adaptive_connectivity_enabled";
 
         /**
+         * Boolean value whether to link ringtone and notification volume
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
