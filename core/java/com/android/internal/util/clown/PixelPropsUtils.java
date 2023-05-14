b8851c72a44d8818348ea57f0114097550255c01
Pranav Vashi <neobuddy89@gmail.com>
2023-05-14 19:52:32 +0530

    PixelPropsUtils: Update supported pixel code names
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- core/java/com/android/internal/util/clown/PixelPropsUtils.java
+++ core/java/com/android/internal/util/clown/PixelPropsUtils.java
@@ -154,6 +154,7 @@ public class PixelPropsUtils {
 
     // Codenames for currently supported Pixels by Google
     private static final String[] pixelCodenames = {
+            "lynx",
             "cheetah",
             "panther",
             "bluejay",
@@ -162,9 +163,7 @@ public class PixelPropsUtils {
             "barbet",
             "redfin",
             "bramble",
-            "sunfish",
-            "coral",
-            "flame"
+            "sunfish"
     };
 
     private static volatile boolean sIsGms = false;
