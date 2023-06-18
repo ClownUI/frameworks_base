9656b6d666e09d62cdeba711b056a1d3489a6b5f
Jesse Chan <jc@lineageos.org>
2023-06-18 12:23:36 +0530

    SystemUI: support black theme for dark mode [1/3]
    Author: Jesse Chan <jc@lineageos.org>
    Date:   Tue May 26 21:17:59 2020 +0800
    
        SystemUI: support black theme for dark mode [1/3]
    
        Change-Id: I57cd53de8f2c1e4d445441b514875b6af915b858
        Signed-off-by: Jesse Chan <jc@lineageos.org>
    
    Author: Michael Bestas <mkbestas@gmail.com>
    Date:   Wed Aug 24 21:19:57 2022 +0300
    
        Don't skip theme application if it's already current
    
        This fixes black theme and font/icon shape customizations.
    
        Change-Id: I8c3d3b4634e1ac60d28e4d5ef20bf9c2015e2e18
    
    Change-Id: I7ebc7064a538c5a5c6adfd05afede7a44e4856b0
    Signed-off-by: HeroBuxx <me@herobuxx.me>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12542,6 +12542,12 @@ public final class Settings {
                 "com.android.server.display.HBM_SETTING_KEY";
 
         /**
+         * Whether to use black theme for dark mode
+         * @hide
+         */
+        public static final String BERRY_BLACK_THEME = "berry_black_theme";
+        
+        /**
          * Control whether to hide ADB and Developer settings enable status.
          * @hide
          */
