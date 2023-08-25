099bd4a8547d9dc20182fffdc90c842e1ef7f694
Dmitrii <bankersenator@gmail.com>
2023-08-25 05:37:02 +0000

    fwb: silence batteryexternalstatsworker spam
    BatteryExternalStatsWorker: error reading Bluetooth stats
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- services/core/java/com/android/server/stats/pull/StatsPullAtomService.java
+++ services/core/java/com/android/server/stats/pull/StatsPullAtomService.java
@@ -1844,7 +1844,7 @@ public class StatsPullAtomService extends SystemService {
 
                         @Override
                         public void onBluetoothActivityEnergyInfoError(int errorCode) {
-                            Slog.w(TAG, "error reading Bluetooth stats: " + errorCode);
+                            //Slog.w(TAG, "error reading Bluetooth stats: " + errorCode);
                             Bundle bundle = new Bundle();
                             bundle.putParcelable(
                                     BatteryStats.RESULT_RECEIVER_CONTROLLER_KEY, null);
@@ -2273,7 +2273,7 @@ public class StatsPullAtomService extends SystemService {
 
                         @Override
                         public void onError(TelephonyManager.ModemActivityInfoException e) {
-                            Slog.w(TAG, "error reading modem stats:" + e);
+                            //Slog.w(TAG, "error reading modem stats:" + e);
                             modemFuture.complete(null);
                         }
                     });
