0fb215b8a7bfd88a6d6aed6f926f20faa72409d7
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-09-05 17:03:03 +0530

    display: Consider peak refresh rate in BrightnessObserver
    If the peak refresh rate set by the user is lower than the low/high
    zone refresh set in the device overlay, it switches to 60Hz. This is
    not ideal, as for example, the user may have set the peak refresh rate
    to 90Hz (while the overlay defines 120Hz as the refresh rate in the low
    brightness zone), but at low brightness the display enters 60Hz instead
    of staying at 90Hz, leading to poor UX.
    
    Use the minimum of the user-set peak brightness and the predefined
    refresh rate of the brightness zone to avoid this behaviour.
    
    Change-Id: Ia69fd5e8221bedb05b958d1eedebb4823ad4a9b7
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- services/core/java/com/android/server/display/mode/DisplayModeDirector.java
+++ services/core/java/com/android/server/display/mode/DisplayModeDirector.java
@@ -1728,6 +1728,7 @@ public class DisplayModeDirector {
         @Nullable
         private SparseArray<RefreshRateRange> mHighZoneRefreshRateForThermals;
         private int mRefreshRateInHighZone;
+        private int mPeakRefreshRate;
 
         @GuardedBy("mLock")
         private @Temperature.ThrottlingStatus int mThermalStatus = Temperature.THROTTLING_NONE;
@@ -1973,10 +1974,16 @@ public class DisplayModeDirector {
         @VisibleForTesting
         public void onRefreshRateSettingChangedLocked(float min, float max) {
             boolean changeable = (max - min > 1f && max > 60f);
-            if (mRefreshRateChangeable != changeable) {
+            int peakRefreshRate = Math.round(Math.max(min, max));
+            if (mRefreshRateChangeable != changeable || mPeakRefreshRate != peakRefreshRate) {
                 mRefreshRateChangeable = changeable;
-                updateSensorStatus();
-                if (!changeable) {
+                mPeakRefreshRate = peakRefreshRate;
+                if (changeable) {
+                    synchronized (mLock) {
+                        onBrightnessChangedLocked();
+                    }
+                } else {
+                    updateSensorStatus();
                     removeFlickerRefreshRateVotes();
                 }
             }
@@ -2325,6 +2332,7 @@ public class DisplayModeDirector {
                 return;
             }
 
+            int refreshRateInLowZone = Math.min(mPeakRefreshRate, mRefreshRateInLowZone);
             boolean insideLowZone = hasValidLowZone() && isInsideLowZone(mBrightness, mAmbientLux);
             if (insideLowZone) {
                 if (hasLowLightVrrConfig()) {
@@ -2332,7 +2340,7 @@ public class DisplayModeDirector {
                             .getRefreshRateData().lowLightBlockingZoneSupportedModes);
                 } else {
                     refreshRateVote = Vote.forPhysicalRefreshRates(
-                            mRefreshRateInLowZone, mRefreshRateInLowZone);
+                            refreshRateInLowZone, refreshRateInLowZone);
                     refreshRateSwitchingVote = Vote.forDisableRefreshRateSwitching();
                 }
                 if (mLowZoneRefreshRateForThermals != null) {
@@ -2346,12 +2354,12 @@ public class DisplayModeDirector {
                 }
             }
 
+            int refreshRateInHighZone = Math.min(mPeakRefreshRate, mRefreshRateInHighZone);
             boolean insideHighZone = hasValidHighZone()
                     && isInsideHighZone(mBrightness, mAmbientLux);
             if (insideHighZone) {
                 refreshRateVote =
-                        Vote.forPhysicalRefreshRates(mRefreshRateInHighZone,
-                                mRefreshRateInHighZone);
+                        Vote.forPhysicalRefreshRates(refreshRateInHighZone, refreshRateInHighZone);
                 if (mHighZoneRefreshRateForThermals != null) {
                     RefreshRateRange range = SkinThermalStatusObserver
                             .findBestMatchingRefreshRateRange(mThermalStatus,
