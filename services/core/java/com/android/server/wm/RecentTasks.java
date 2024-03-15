ClownUI/frameworks_base

commit daab21943d76dc1086036c9b0825a0d284376c04

Keeps the task relative z-ordering when moving an existing recent task
In multi-window scenario, multiple tasks are resumed from the top down
and adding the tasks are added to recents respectively. Therefore,
the task order in the recents list is reversed.

Instead of always moving the existing task to top in recents, this CL
finds a proper index and takes the actual task hierarchy into account
to move the task in recents.


Bug: 317488684
Test: manually verified in desktop windowing
Change-Id: Ib4286340ce83810655fefad598cc37968f93a77d

services/core/java/com/android/server/wm/RecentTasks.java
@@ -1135,16 +1135,17 @@ class RecentTasks {
                     if (!mFreezeTaskListReordering) {
                         // Simple case: this is not an affiliated task, so we just move it to the
                         // front unless overridden by the provided activity options
+                        int indexToAdd = findIndexToAdd(task);
                         mTasks.remove(taskIndex);
-                        mTasks.add(0, task);
+                        mTasks.add(indexToAdd, task);
                         if (taskIndex != 0) {
                             // Only notify when position changes
                             mTaskNotificationController.notifyTaskListUpdated();
                         }
 
                         if (DEBUG_RECENTS) {
-                            Slog.d(TAG_RECENTS, "addRecent: moving to top " + task
-                                    + " from " + taskIndex);
+                            Slog.d(TAG_RECENTS, "addRecent: moving " + task + " to index "
+                                    + indexToAdd + " from " + taskIndex);
                         }
                     }
                     notifyTaskPersisterLocked(task, false);
@@ -1229,6 +1230,37 @@ class RecentTasks {
 
         mCheckTrimmableTasksOnIdle = true;
         notifyTaskPersisterLocked(task, false /* flush */);
+    }
+
+    // Looks for a new index to move the recent Task. Note that the recent Task should not be
+    // placed higher than another recent Task that has higher hierarchical z-ordering.
+    private int findIndexToAdd(Task task) {
+        int indexToAdd = 0;
+        for (int i = 0; i < mTasks.size(); i++) {
+            final Task otherTask = mTasks.get(i);
+            if (task == otherTask) {
+                break;
+            }
+
+            if (!otherTask.isAttached()) {
+                // Stop searching if not attached.
+                break;
+            }
+
+            if (otherTask.inPinnedWindowingMode()) {
+                // Skip pip task without increasing index since pip is always on screen.
+                continue;
+            }
+
+            // Stop searching if the task has higher z-ordering, or increase the index and
+            // continue the search.
+            if (task.compareTo(otherTask) > 0) {
+                break;
+            }
+
+            indexToAdd = i + 1;
+        }
+        return indexToAdd;
     }
 
     /**

commit ab932b455722fbc5a43ebe4a59ce17ee8a49ff81

Fix MixedDeviceOwnerTest test failure
NullPointerException was thrown while running the test because
the top activity of the most recent task is null. This is a
regression caused by commit daab219.

It happens when adding a recent task because of resuming the next
activity while the top-most activity finishes (the activities are
in two different Tasks). The finishing task still has the higher
z-ordering in the WM hierarchy which therefore was remained be the
most recent task.

The finishing task should be excluded and the most recent task
should be the one that is resumed.

Bug: 326367294
Test: atest RecentTasksTest
Test: atest MixedDeviceOwnerTest
Change-Id: Iba8a2004a58a4d440a0c62c59ae89b5e4ca1253c

services/core/java/com/android/server/wm/RecentTasks.java
@@ -1258,6 +1258,11 @@ class RecentTasks {
                 continue;
             }
 
+            if (otherTask.topRunningActivity() == null) {
+                // Skip if there's no running activity in the Task.
+                continue;
+            }
+
             // Stop searching if the task has higher z-ordering, or increase the index and
             // continue the search.
             if (task.compareTo(otherTask) > 0) {

services/tests/wmtests/src/com/android/server/wm/RecentTasksTest.java
@@ -1210,9 +1210,11 @@ public class RecentTasksTest extends WindowTestsBase {
     @Test
     public void addTask_tasksAreAddedAccordingToZOrder() {
         final Task firstTask = new TaskBuilder(mSupervisor).setTaskId(1)
-                .setWindowingMode(WINDOWING_MODE_FREEFORM).build();
+                .setWindowingMode(WINDOWING_MODE_FREEFORM)
+                .setCreateActivity(true).build();
         final Task secondTask = new TaskBuilder(mSupervisor).setTaskId(2)
-                .setWindowingMode(WINDOWING_MODE_FREEFORM).build();
+                .setWindowingMode(WINDOWING_MODE_FREEFORM)
+                .setCreateActivity(true).build();
 
         assertEquals(-1, firstTask.compareTo(secondTask));
 