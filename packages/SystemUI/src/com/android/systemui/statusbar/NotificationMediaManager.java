55762da745f9ad5081170a37fd70c658355a91d9
Pranav Vashi <neobuddy89@gmail.com>
2024-03-25 21:34:30 +0530

    SystemUI: IslandView: Properly handle island notification on track change
    * We need to cancel exisiting notification for new one to popup.
    
    Change-Id: I10a1500e8791c3576e346aacccce337f08328fbf
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -154,11 +154,13 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
                 setMediaMetadata(metadata);
             }
             if (mIslandEnabled && mIslandNowPlayingEnabled) {
+                notifUtils.cancelNowPlayingNotification();
                 if (mStatusBarStateController.getState() != KEYGUARD 
-                        && !mStatusBarStateController.isDozing()) {
+                        && !mStatusBarStateController.isDozing()
+                        && PlaybackState.STATE_PLAYING == getMediaControllerPlaybackState(mMediaController) 
+                        && mMediaMetadata != null) {
                     notifUtils.showNowPlayingNotification(metadata);
                 }
-                notifUtils.cancelNowPlayingNotification();
             }
 
             dispatchUpdateMediaMetaData();
