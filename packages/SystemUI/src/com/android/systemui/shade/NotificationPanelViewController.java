716dd7cfde98bac7cb213efc7d0679583a097cd9
Pranav Vashi <neobuddy89@gmail.com>
2024-04-30 09:35:20 +0530

    SystemUI: IslandView: Update color implementation
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -1472,12 +1472,6 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         return view;
     }
 
-    void updateIslandBackground() {
-        boolean nightMode = (mView.getContext().getResources().getConfiguration().uiMode
-                & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES;
-        mNotifIsland.setIslandBackgroundColorTint(nightMode);
-    }
-
     @VisibleForTesting
     void reInflateViews() {
         debugLog("reInflateViews");
@@ -4572,7 +4566,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         public void onThemeChanged() {
             debugLog("onThemeChanged");
             reInflateViews();
-            updateIslandBackground();
+            mNotifIsland.setIslandBackgroundColorTint();
         }
 
         @Override
