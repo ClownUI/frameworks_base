375884bd506b12bae5c40055c835a56a9305f4a3
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-14 08:49:33 +0800

    SystemUI: IslandView: Clean up redundant enablement
    neobuddy89: This is controlled NPVC, so it can be dropped in IslandView.
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -4787,11 +4787,9 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
             switch (key) {
                 case ISLAND_NOTIFICATION:
                     mUseIslandNotification = TunerService.parseIntegerSwitch(newValue, false);
-                    mNotifIsland.setIslandEnabled(mUseIslandNotification && mUseHeadsUp);
                     break;
                 case HEADS_UP_NOTIFICATIONS_ENABLED:
                     mUseHeadsUp = TunerService.parseIntegerSwitch(newValue, true);
-                    mNotifIsland.setIslandEnabled(mUseIslandNotification && mUseHeadsUp);
                     break;
                 default:
                     break;
