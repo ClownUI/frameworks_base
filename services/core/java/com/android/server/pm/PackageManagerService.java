0d1d45012b8ab48d4dc58e3ac82f38672451d540
gang.huang <gang.huang@unisoc.com>
2024-10-25 18:07:49 +0800

    Ignore null action in PackageManagerService.
    Bug: 375569842
    Test: Manual.
    Google: 3320013
    Change-Id: I85cb675a456a058a3c54689077dc21e233337d2c
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4280,6 +4280,10 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
                 if (intent == null) {
                     return;
                 }
+                final String action = intent.getAction();
+                if (action == null) {
+                    return;
+                }
                 Uri data = intent.getData();
                 if (data == null) {
                     return;
